0df05014e2b17438cb3f8ddff0e3190ebe3caf95
techieritesh <riteshponty@gmail.com>
2023-05-27 17:00:26 +0530

    stack question2

--- src/Stack/Maximal_Rectangle.java
+++ src/Stack/Maximal_Rectangle.java
@@ -2,12 +2,34 @@ package Stack;
 
 import java.util.Stack;
 
-public class Largest_Rectangle_In_Histogram {
+public class Maximal_Rectangle {
     public static void main(String[] args) {
-        int arr[] = {2, 3, 5, 4, 6, 1, 7};
-        System.out.println(find(arr));
+        int arr[][]={{1,0,1,0,0},{1,0,1,1,1},{1,1,1,1,1},{1,0,0,1,0}};
+        int histo[]=new int[arr[0].length];
+        int area=0;
+        for (int i = arr.length-1; i >=0 ; i--) {
+            if (i == arr.length - 1) {
+                for (int j = 0; j < arr[0].length; j++) {
+                    histo[j] = arr[i][j];
+                }
+            } else {
+                for (int j = 0; j < arr[0].length; j++) {
+                    if(arr[i][j]==0){
+                        histo[j]=0;
+                    }else{
+                    histo[j] += arr[i][j];
+                }
+                }
+                area=Math.max(area,find(histo));
+//                System.out.println(area);
+//                System.out.println("---------------------");
+//                for (int j = 0; j < histo.length; j++) {
+//                    System.out.println(histo[j]);
+//                }
+            }
+        }
+        System.out.println(area);
     }
-
     public static int find(int arr[]) {
         Stack<Integer> st = new Stack<>();
         int maxarea = 0;
