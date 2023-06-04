fcaab61e5956380acd460d4074f1e5e1c0d41c98
techieritesh <riteshponty@gmail.com>
2023-06-04 21:26:11 +0530

    updates

--- src/LinkedList/Sort_LinkedList.java
+++ src/LinkedList/Sort_LinkedList.java
@@ -10,8 +10,9 @@ public class Sort_LinkedList {
     }
     public static void main(String args[]){
         Node head=new Node(10);
-
-
-
+        head.next=new Node(20);
+        head.next.next=new Node(30);
+        head.next.next.next=new Node(40);
+        head.next.next.next.next=new Node(50);
     }
 }
