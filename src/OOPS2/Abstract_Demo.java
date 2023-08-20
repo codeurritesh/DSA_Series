c0c832eff987cedcc855e560398e4e00ee2fd170
techieritesh <riteshponty@gmail.com>
2023-08-20 15:09:32 +0530

    Oppes 2

---
+++ src/OOPS2/Abstract_Demo.java
@@ -0,0 +1,11 @@
+public  abstract class Abstract_Demo {
+
+	public abstract int fun();
+
+	public void fun1() {
+
+	}
+
+
+
+}
---
+++ src/OOPS2/BankAccount.java
@@ -0,0 +1,9 @@
+public abstract class BankAccount {
+
+	public  abstract boolean payment() ;
+
+	public  int accountbalnec() {
+		return 1000;
+	}
+
+}
